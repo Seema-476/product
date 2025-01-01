6b0eedeae35d8974421b8148de5cf09f0da55d6f
Seema-476 <simmybishnoi29085@gmail.com>
2025-01-01 10:13:39 +0530

    changes

--- src/common/Heading.jsx
+++ src/common/Heading.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Heading = ({text}) => {
   return (
-      <h2 className='product font-semibold mb-0'>{text }</h2>
+      <h2 className='product text-white font-semibold mb-0'>{text }</h2>
   )
 }
 
--- src/components/about/Counter.jsx
+++ src/components/about/Counter.jsx
@@ -29,10 +29,10 @@ const Counter = () => {
     }, []);
 
   return (
-      <div className='container max-w-1440 pt-5'>
-          <h3 className='product mb-0 mx-auto text-center'>Usestate</h3>
+      <div className='container container-width pt-5'>
+          <h3 className='product mb-0 mx-auto text-center text-white'>Usestate</h3>
           <div className='text-center'>
-              <p className='description mb-0 text-center mx-auto pb-3'>{ value}</p>
+              <p className='description mb-0 text-center mx-auto pb-3 text-white'>{ value}</p>
               <div className='flex justify-center gap-2'>
                   <button className='p-2' onClick={increment}>Increment</button>
                   <button className='p-2' onClick={decrement}>decrement</button>
@@ -40,13 +40,13 @@ const Counter = () => {
           </div>
 
           <div className='text-center'>
-              <p className='product pt-5 mb-0'>UseEffect</p>
-              <p className='description mb-0 text-center mx-auto'>{count}</p>
+              <p className='product pt-5 mb-0 text-white'>UseEffect</p>
+              <p className='description mb-0 text-center mx-auto text-white'>{count}</p>
           </div>
 
           <div className='text-center'>
-              <p className='product pt-5 mb-0 '>For Loop</p>
-              <p className='description mb-0 text-center mx-auto'>{number}</p>
+              <p className='product pt-5 mb-0 text-white'>For Loop</p>
+              <p className='description mb-0 text-center mx-auto text-white'>{number}</p>
           </div>
     </div>
   )
